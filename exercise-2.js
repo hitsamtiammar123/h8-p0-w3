66e5b557eecd6fd1f88a1eca5129d036ec67138b
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2019-12-16 12:25:03 +0700

    add exercise-2.js

---
+++ exercise-2.js
@@ -0,0 +1,11 @@
+function balikKata(str){
+    var newStr='';
+    for(var i=str.length-1;i>=0;i--){
+        newStr=newStr+str[i];
+    }
+
+    return newStr;
+}
+
+console.log(balikKata('hello world!'));
+
