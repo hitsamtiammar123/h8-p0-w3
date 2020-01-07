a7827845cc52adb8aa67402d7c441b6f1a14e244
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2020-01-07 16:11:54 +0700

    perbaikan exercise-15.js

--- exercise-15.js
+++ exercise-15.js
@@ -1,7 +1,23 @@
+function unique(arr){
+    var newArr=[];
+    for(var i=0;i<arr.length;i++){
+        var c1=arr[i];
+        var counter=0
+        for(var j=0;j<arr.length && j!==i;j++){
+            var c2=arr[j];
+            if(c1==c2)
+                counter++;
+        }
+        if(counter===0)
+            newArr.push(c1);
+    }
+    return newArr;
+}
+
 function groupAnimals(animals) {
     // you can only write your code here!
     var map_arr=animals.map((c) => c[0]);
-    var sort_arr=Array.from(new Set(map_arr)).sort();
+    var sort_arr=unique(map_arr).sort();
 
     var result=[];
     for(var i=0;i<sort_arr.length;i++){
